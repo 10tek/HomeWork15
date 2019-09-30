cdaacb70f137948a369fd4892c7aa308258e7170
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-01 04:44:57 +0600

    Добавил класс User и получение значения объекта с помощью рефлексии

---
+++ HW15/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace HW15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = typeof(Console).Assembly;
+            var types = assembly.GetTypes();
+            var methodList = new List<string>();
+            foreach (var type in types)
+            {
+                if (type.Name == "Console")
+                {
+                    foreach (var method in type.GetMethods())
+                    {
+                        if (!methodList.Contains(method.Name))
+                        {
+                            methodList.Add(method.Name);
+                        }
+                    }
+                }
+            }
+            foreach(var method in methodList)
+            {
+                Console.WriteLine(method);
+            }
+
+            User user = new User()
+            {
+                Name = "Tomy",
+                Login = "Shelby",
+                Age = 33
+            };
+
+            Console.WriteLine("\n2.");
+            assembly = typeof(User).Assembly;
+            types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                if (type.Name == "User")
+                {
+                    foreach (var member in type.GetMembers())
+                    {
+                        if (member is PropertyInfo)
+                        {
+                            var propertyInfo = member as PropertyInfo;
+                            Console.WriteLine($"{propertyInfo.Name} - {propertyInfo.GetValue(user)}");
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ HW15/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW15
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public int Age { get; set; }
+    }
+}
